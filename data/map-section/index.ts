d406fe750cb055226c86be292c78bcda9ca1f361
Muhammad Inzamam Shakir <mian.inzy535@gmail.com>
2024-11-24 01:03:17 +0500

    "Updated Google Maps embed URL and removed trailing newline"

--- data/map-section/index.ts
+++ data/map-section/index.ts
@@ -2,8 +2,7 @@ import { MapSectionProps } from '@/src/sections/map-section';
 
 export const mapSectionData: MapSectionProps = {
   embedUrl:
-    'https://www.google.com/maps/d/embed?mid=1Chdjwy45ToMNucDSfO-bkRgeebZAw60&ehbc=2E312F',
+    'https://www.google.com/maps/d/u/1/embed?mid=1Chdjwy45ToMNucDSfO-bkRgeebZAw60&ehbc=2E312F',
 };
-
 
 // Drive Link  for Google Maps Embed https://www.google.com/maps/d/edit?mid=1Chdjwy45ToMNucDSfO-bkRgeebZAw60&usp=sharing
